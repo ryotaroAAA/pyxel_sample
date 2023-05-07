f4e0234ae78cfc0e1121719783f24f61b21781a4
ryotaro <ryo.ex3991@gmail.com>
2023-05-08 01:13:39 +0900

    マップ周り作成

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 import pyxel
+import pprint
 from enum import auto, Enum
 
 class Game:
     tile_dic = {
         # background
-        "wall" : (0, 6, None),
-        "water" : (2, 6, None),
-        "ailse" : (0, 5, None),
-        "grass" : (2, 5, None),
-        "wood" : (3, 5, None),
+        "wall" : (0, 6, None, 1),
+        "water" : (2, 6, None, 1),
+        "aisle" : (0, 5, None, 0),
+        "grass" : (2, 5, None, 0),
+        "wood" : (3, 5, None, 1),
         # sprite
         # charactor
-        "hero" : (0, 0, pyxel.colors[0]),
-        "reimu" : (0, 1, pyxel.colors[1]),
-        "marisa" : (0, 2, pyxel.colors[1]),
+        "hero" : (1, 0, 0, 1),
+        "reimu" : (2, 0, 1, 1),
+        "marisa" : (3, 0, 1, 1),
         # item
-        "wepon1" : (0, 1, pyxel.colors[0]),
-        "wepon2" : (1, 1, pyxel.colors[0]),
-        "wepon3" : (2, 1, pyxel.colors[0]),
-        "wepon4" : (3, 1, pyxel.colors[0]),
-        "wepon5" : (4, 1, pyxel.colors[0]),
-        "armor1" : (0, 2, pyxel.colors[0]),
-        "armor2" : (1, 2, pyxel.colors[0]),
-        "armor3" : (2, 2, pyxel.colors[0]),
-        "item1" : (0, 3, pyxel.colors[0]),
-        "item2" : (1, 3, pyxel.colors[0]),
-        "item3" : (2, 3, pyxel.colors[0]),
-        "item4" : (3, 3, pyxel.colors[0]),
-        "treasure" : (4, 3, pyxel.colors[0]),
+        "wepon1" : (0, 1, 0, 1),
+        "wepon2" : (1, 1, 0, 1),
+        "wepon3" : (2, 1, 0, 1),
+        "wepon4" : (3, 1, 0, 1),
+        "wepon5" : (4, 1, 0, 1),
+        "armor1" : (0, 2, 0, 1),
+        "armor2" : (1, 2, 0, 1),
+        "armor3" : (2, 2, 0, 1),
+        "item1" : (0, 3, 0, 1),
+        "item2" : (1, 3, 0, 1),
+        "item3" : (2, 3, 0, 1),
+        "item4" : (3, 3, 0, 1),
+        "treasure" : (4, 3, 0, 1),
         # enemy
-        "enemy1" : (0, 4, pyxel.colors[0]),   
-        "enemy2" : (1, 4, pyxel.colors[0]),    
-        "enemy3" : (2, 4, pyxel.colors[0]),   
-        "enemy4" : (3, 4, pyxel.colors[0]),    
-        "boss" : (4, 4, pyxel.colors[0])    
+        "enemy1" : (0, 4, 0, 1),
+        "enemy2" : (1, 4, 0, 1),
+        "enemy3" : (2, 4, 0, 1),
+        "enemy4" : (3, 4, 0, 1),
+        "boss" : (4, 4, 0, 1)
     }
     def __init__(self):
         self.tile_x = 8
@@ -42,22 +43,44 @@ class Game:
         self.bit_y = 8
         self.size_x = self.tile_x*self.bit_x
         self.size_y = self.tile_y*self.bit_y
-        pyxel.init(self.size_x, self.size_y, fps=30)
+        pyxel.init(self.size_x, self.size_y, fps = 30)
 
-        # colors = pyxel.colors.to_list()
-        # print(colors)
-        pyxel.load("test.pyxres")
+        colors = pyxel.colors.to_list()
+        print(colors)
+        pyxel.load("assets.pyxres")
 
         # start position
         self.x = 2
         self.y = 2
+
+        self.map_size_x = 24
+        self.map_size_y = 24
+        self.map_init()
         pyxel.run(self.update, self.draw)
 
     # 初期化
     # マップ、当たり判定、位置、
-    # def map_init(self):
+    def map_init(self):
+        self.map = [[0 for _ in range(self.map_size_x)]
+            for _ in range(self.map_size_y)]
+        for i in range(self.map_size_x):
+            for j in range(self.map_size_y):
+                x, y = pyxel.tilemap(0).pget(i, j)
+                for _, tile in self.tile_dic.items():
+                    if tile[0] == x and tile[1] == y:
+                        self.map[j][i] = tile
 
-    # def is_passable(self):
+    def is_collision(self, x, y):
+        # tile_x, tile_y = pyxel.tilemap(0).pget(
+        #     x + self.tile_x * (self.x//self.tile_x),
+        #     y + self.tile_y * (self.y//self.tile_y))
+        # collision = 0
+        # for _, tile in self.tile_dic.items():
+        #     if tile[0] == tile_x and tile[1] == tile_y:
+        #         collision = tile[3]
+        #         break
+        # # print(x, y, tile_x, tile_y, tile, collision)
+        return self.map[y][x][3]
 
     def draw_tile(self, x, y, tile):
         """
@@ -67,59 +90,58 @@ class Game:
         tile_x = tile[0]
         tile_y = tile[1]
         colkey = tile[2]
-        # print(tile, tile_x, tile_y, pyxel.tilemap(0).pget(tile_x, tile_y))
-        if not colkey == None:
-            pyxel.blt(x*self.bit_x,
-                y*self.bit_y,
-                0,
-                tile_x*self.bit_x,
-                tile_y*self.bit_y,
-                self.bit_x,
-                self.bit_y,
-                colkey)
-        else:
-            pyxel.blt(x*self.bit_x,
-                y*self.bit_y,
-                0,
-                tile_x*self.bit_x,
-                tile_y*self.bit_y,
-                self.bit_x,
-                self.bit_y)
+        # print(x, y, tile, tile_x, tile_y, pyxel.tilemap(0).pget(tile_x, tile_y))
+        pyxel.blt(x*self.bit_x,
+            y*self.bit_y,
+            0,
+            tile_x*self.bit_x,
+            tile_y*self.bit_y,
+            self.bit_x,
+            self.bit_y,
+            colkey)
 
     # 毎フレームオンメモリ情報を書き換える
     def update(self):
         # self.x = (self.x + 1) % pyxel.width
         if pyxel.frame_count % 5 == 0:
+            x = self.x
+            y = self.y
+            mod = False
             if pyxel.btn(pyxel.KEY_UP):
-                self.y = self.y - 1
+                y = y - 1
+                mod = True
             if pyxel.btn(pyxel.KEY_DOWN):
-                self.y = self.y + 1
+                y = y + 1
+                mod = True
             if pyxel.btn(pyxel.KEY_LEFT):
-                self.x = self.x - 1
+                x = x - 1
+                mod = True
             if pyxel.btn(pyxel.KEY_RIGHT):
-                self.x = self.x + 1
+                x = x + 1
+                mod = True
+            if mod and not self.is_collision(x, y):
+                self.x = x
+                self.y = y
     
     # 毎フレーム実際描画する
     def draw(self):
         # 画面黒初期化
         pyxel.cls(0)
-
+        pyxel.camera()
+        
         # draw background
-        for i in range(0, self.tile_x):
-            for j in range(0, self.tile_y):
-                tile_x, tile_y = pyxel.tilemap(0).pget(
-                    i + self.tile_x * (self.x//self.tile_x),
-                    j + self.tile_y * (self.y//self.tile_y))
-                for k, tile in self.tile_dic.items():
-                    # if i == 7:
-                    if k == "wall":
-                        print(i, j, tile_x, tile_y, k, tile)
-                    if tile[0] == tile_x and tile[1] == tile_y:
-                        self.draw_tile(i, j, tile)
+        opt_x = max(self.x - self.tile_x//2, 0)
+        opt_x = min(opt_x, self.map_size_x - self.tile_x)
+        opt_y = max(self.y - self.tile_y//2, 0)
+        opt_y = min(opt_y, self.map_size_y - self.tile_y)
+        pyxel.camera(opt_x*self.bit_x, opt_y*self.bit_y)
+        
+        for i in range(self.map_size_x):
+            for j in range(self.map_size_y):
+                # print(self.x, self.y, i, j, opt_x, opt_y)
+                self.draw_tile(i, j, self.map[j][i])
         
         # draw sprites
-        self.draw_tile(self.x % self.tile_x,
-                       self.y % self.tile_y,
-                       self.tile_dic["hero"])
+        self.draw_tile(self.x, self.y, self.tile_dic["reimu"])
 
 Game()
